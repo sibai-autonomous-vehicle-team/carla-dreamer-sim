aed69583335221634f1fe652b4960f0f7672714a
ahmad8743 <a.h.hamzeh@wustl.edu>
2025-06-21 16:57:54 -0500

    Preliminary methods written; image observations captured

--- carla_wrapper.py
+++ carla_wrapper.py
@@ -1,5 +1,26 @@
 from car_dreamer.carla_follow_env import CarlaFollowEnv
 import numpy as np
+import torch
+
+def aggregate_dct(dcts):
+    full_dct = {}
+    for dct in dcts:
+        for key, value in dct.items():
+            if key not in full_dct:
+                full_dct[key] = []
+            full_dct[key].append(value)
+    for key, value in full_dct.items():
+        if isinstance(value[0], torch.Tensor):
+            full_dct[key] = torch.stack(value)
+        else:
+            full_dct[key] = np.stack(value)
+    return full_dct
+
+def get_image_obs(obs):
+    """
+    Extract image observation from the observation dictionary.
+    """
+    return obs['camera'] if 'camera' in obs and len(obs['camera']) > 0 else None
 
 class CarlaWrapper(CarlaFollowEnv):
     def __init__(self, config):
@@ -20,6 +41,7 @@ class CarlaWrapper(CarlaFollowEnv):
         """
         self.reset_to_state = reset_to_state
         obs = super().reset()
+        obs = get_image_obs(obs)
         return obs, self.state()
     
     def update_env(self, env_info):
@@ -34,7 +56,6 @@ class CarlaWrapper(CarlaFollowEnv):
         Evaluate the current state against the goal state.
         This method can be used to determine how close the current state is to the goal state.
         """
-        # Example evaluation: return the Euclidean distance between the two states
         cur_ego_pos = np.array(cur_state[0][:2])
         cur_nonego_pos = np.array(cur_state[1][:2])
         cur = np.concatenate((cur_ego_pos, cur_nonego_pos))
@@ -61,4 +82,41 @@ class CarlaWrapper(CarlaFollowEnv):
         self.seed(seed)
         return self.reset(init_state)
     
+    def step_multiple(self, actions):
+        """
+        Perform multiple steps in the environment with the given actions.
+        This method can be used to execute a sequence of actions in the environment.
+        """
+        observations = []
+        rewards = []
+        dones = []
+        infos = []
+        for action in actions:
+            o, r, d, info = self.step(action)
+            o = get_image_obs(o)
+            obses.append(o)
+            rewards.append(r)
+            dones.append(d)
+            infos.append(info)
+        obses = aggregate_dct(obses)
+        rewards = np.stack(rewards)
+        dones = np.stack(dones)
+        infos = aggregate_dct(infos)
+        return obses, rewards, dones, infos
     
+    def rollout(self, seed, init_state, actions):
+        """
+        Perform a rollout in the environment with the given seed, initial state, and actions.
+        This method can be used to execute a sequence of actions and collect observations, rewards, and other information.
+        """
+        obs, state = self.prepare(seed, init_state)
+        obs = get_image_obs(obs)
+
+        obses, rewards, dones, infos = self.step_multiple(actions)
+        for k in obses.keys():
+            obses[k] = np.vstack([np.expand_dims(obs[k], 0), obses[k]])
+        states = np.vstack([np.expand_dims(state, 0), infos["state"]])
+        states = np.stack(states)
+        return obses, states
+
+
--- simulator.py
+++ simulator.py
@@ -1,8 +1,7 @@
-import carla
 import gym
-import yaml
 import car_dreamer
-from car_dreamer.carla_follow_env import CarlaFollowEnv
+from carla_wrapper import get_image_obs
+import numpy as np
 
 
 # Initialize the environment
@@ -14,7 +13,7 @@ env = gym.make('CarlaFollowEnv-v0', config=config.env)
 #     [-0.8586273193359375, -134.7899169921875, -0.008075752295553684, 0.06346611678600311, 1.2272632122039795, -5.211023626494615e-11]
 # ]
 
-observation = env.reset()
+observation = get_image_obs(env.reset())
 
 episode_count = 0
 step_index = 0
@@ -22,26 +21,25 @@ dataset = {
     'states': [],  # list of states
     'observations': [],  # list of obs
     'actions': [],
-    'cost': []
+    'costs': []
 }
 
 
 print('Collect Initial Data')
-for i in range(500):
+for i in range(50):
     action = env.compute_continuous_action() # Sample a random action
     next_obs, reward, terminated, info = env.step(action)
 
-    dataset['states'].append(env.state())
-    dataset['observations'].append(observation)
+    dataset['states'].append(np.array(env.state()))
+    dataset['observations'].append(np.array(get_image_obs(observation)))
     dataset['actions'].append(action)
-    dataset['cost'].append(int(env.is_collision()))
+    dataset['costs'].append(int(env.is_collision()))
 
     print(f"Step {i} - Reward: {reward}, Cost: {int(env.is_collision())} Terminated: {terminated} Action: {action}")
     observation = next_obs
     if terminated:
         obs = env.reset()
-print(dataset['states'][0], dataset['states'][1])
-print(len(dataset['states']), len(dataset['observations']), len(dataset['actions']), len(dataset['cost']))
+print(dataset['observations'][1], dataset['observations'][1].shape)
 
 env.close()
 
